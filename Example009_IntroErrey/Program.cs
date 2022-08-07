c5200227b51dfcd18ebf7cf763c31ca46410e57f
OlesyaDey <vestadey@gmail.com>
2022-08-07 16:50:27 +0300

    Методы для заполнения и вывода массивов

---
+++ Example009_IntroErrey/Program.cs
@@ -0,0 +1,85 @@
+﻿// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+// //               0   1   2   3   4   5   6   7   8
+// int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
+// //array[0] = 12;
+// //Console.WriteLine(array[4]);
+
+// int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+// Console.WriteLine(max);
+
+
+// 
+
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+
+}
+
+//создали новый массив с 10 элементами в нем.
+int[] array = new int[10];
+
+FillArray(array);
+array[4]=4;
+array[6]=4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 5);
+Console.WriteLine(pos);
+
+
+
+
+
+
+
+
+
+
+
+
